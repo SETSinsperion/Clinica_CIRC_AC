a888dacd5e9b758ab23d24dfe25e9789501fc303
Sergio Ernesto Tostado Sánchez <sets.insperion@gmail.com>
2018-12-01 20:17:39 -0600

    [ADD] Add models

---
+++ medical_record/__manifest__.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+##########################################################################
+# Copyright (C) 2018 Insperion
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##########################################################################
+#
+# Developer(s)
+# ------------
+#
+# - Sergio Ernesto Tostado Sánchez (sets.insperion@gmail.com)
+# 
+##########################################################################
+{
+    'name' : 'INSPERION | Medical Records',
+    'version' : '1.0',
+    'summary': 'INSPERION | Medical Records for CIRC A.C Clinic',
+    'sequence': 100,
+    'description': """
+INSPERION | Medical Records
+===========================
+
+Medical Records for CIRC A.C Clinic
+-----------------------------------
+
+    """,
+    'category': 'Sales Management',
+    'website': 'https://www.facebook.com/InsperionInYourWorld/',
+    'depends' : [
+        'base'
+    ],
+    'data': [
+        
+    ],
+    'demo': [
+        
+    ],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
