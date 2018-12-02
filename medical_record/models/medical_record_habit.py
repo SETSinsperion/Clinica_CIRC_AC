d527fd645a133c773d9358b1651196abdbeb4683
Daniel Eusebio García Muñoz <degm.insperion@gmail.com>
2018-12-01 20:57:05 -0600

    [MOD] Habit Model Modified

---
+++ medical_record/models/medical_record_habit.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+##########################################################################
+# Copyright (C) 2018 Insperion
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##########################################################################
+#
+# Developer(s)
+# ------------
+#
+# - Daniel Eusebio García Muñoz (degm.insperion@gmail.com)
+# 
+##########################################################################
+
+from odoo import api, fields, models, _
+
+class MedicalRecordHabit(models.Model):
+
+    _name = 'medical.record.habit'
+    
+    description = fields.Text(
+        string='Habit Description',
+        help='Habit description of the patient.',
+        required=False
+    )
+    type = fields.Selection(
+        [('ali', 'Alimentary'), ('Phy', 'Physical Activities'), ('hobb', 'Hobbies')],
+        string='Type',
+        help='Habit type.',
+        required=True
+    )
