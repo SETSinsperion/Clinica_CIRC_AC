18a0d13ddf9cb8ac8af10ba202188ef4e32dee0a
Sergio Ernesto Tostado Sánchez <sets.insperion@gmail.com>
2019-05-28 14:53:21 -0500

    [ADD][medical_record] Added address to record report.

--- medical_record/models/inherited_res_partner.py
+++ medical_record/models/inherited_res_partner.py
@@ -47,3 +47,25 @@ class ResPartner(models.Model):
                 self.age = round(self.age) + 1
             else:
                 raise UserError(_('There are 12 months in a single year, please type a valid age\n(you typed %s months).') % scale)
+
+    @api.model
+    def getAddress(self):
+        address = ""
+        if not self.street and not self.street2 and not self.city and \
+           not self.state_id and not self.country_id:
+            address = _("Not address.")
+        else:
+            address = '%s%s%s%s%s%s%s%s%s%s%s' % (
+                self.street if self.street else '',
+                ' ' if self.street else '',
+                self.street2 if self.street2 else '',
+                ', ' if self.street or self.street2 else '',
+                _('ZIP %s') % self.zip if self.zip else '',
+                ', ' if self.zip else '',
+                self.city if self.city else '',
+                ', ' if self.city else '',
+                self.state_id.name if self.state_id else '',
+                ', ' if self.state_id else '',
+                self.country_id.name if self.country_id else ''
+            )
+        return address
